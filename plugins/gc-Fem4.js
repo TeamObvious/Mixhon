1d8ddaee2fb174fd0f2b9bc8d99b11af28502d18
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 11:59:40 -1200

    Update and rename gc-infinito.js to gc-Fem4.js

---
+++ plugins/gc-Fem4.js
@@ -0,0 +1,26 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Fem4);
+};
+handler.command = /^(Fem4|fem4)$/i;
+export default handler;
+
+global.Fem4 = `
+🌸 VS OFICIAL FEM - 4 VS 4 🌸
+
+Nota : La Hora Editenlo De Acuerdo El Vs
+
+HORA :
+•10 pm 🇵🇪
+•00 am 🇦🇷 🇨🇱
+
+
+🎀 
+🎀 
+🎀 
+🎀 
+
+🌸 SUPLENTES 🌸
+
+🎀
+🎀 
+`;
