380c1ba9ed3a1cd4485acda3eac41fad7c7a4125
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 11:54:53 -1200

    Update and rename gc-destraba.js to gc-Masc4.js

---
+++ plugins/gc-Masc4.js
@@ -0,0 +1,22 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Masc4);
+};
+handler.command = /^(Masc4|masc4|liga)$/i;
+export default handler;
+
+global.Masc4 = `
+*⚜️ VS OFICIAL MASC - 4 VS 4 ⚜️*
+
+_HORA :_
+•10 Pm 🇵🇪
+
+👺 
+👺 
+👺 
+👺 
+
+*⚠️SUPLENTES⚠️*
+
+👺
+👺 
+`;
