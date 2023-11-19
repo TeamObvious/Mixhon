e7afea9e94a8d76d3b9bdb35771ef2ceebf3398f
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 11:34:44 -1200

    Update gc-vs8.js

--- plugins/gc-vs8.js
+++ plugins/gc-vs8.js
@@ -4,30 +4,32 @@ const handler = async (m, {conn}) => {
 handler.command = /^(Vs8|vs8)$/i;
 export default handler;
 global.vs8 = `
-*⚜️ VS OFICIAL - 8 VS 8⚜️*
+*⚜️ VS OFICIAL MASC - 8 VS 8 ⚜️*
 
-_HORA :_
+*Nota :* _La Hora Editenlo De Acuerdo El Vs_
+
+*HORA :*
 •10 pm 🇵🇪
-•10 pm 🇦🇷🇨🇱
+•00 am 🇦🇷 🇨🇱
 
 *👑 ESCUADRA 1 👑*
 
-📱 
-📱 
-📱 
-📱 
+👺 
+👺 
+👺 
+👺 
 
 *👑 ESCUADRA 2 👑*
 
-📱
-📱
-📱
-📱
+👺
+👺
+👺
+👺
 
 *⚠️SUPLENTES⚠️*
 
-📱 
-📱
-📱
-📱
+👺 
+👺
+👺
+👺
 `;
