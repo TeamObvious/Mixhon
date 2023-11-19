3ac657cf70b605e59b675fd6e2b29bd0359c5891
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 11:27:21 -1200

    Update gc-vs8.js

--- plugins/gc-vs8.js
+++ plugins/gc-vs8.js
@@ -3,14 +3,12 @@ const handler = async (m, {conn}) => {
 };
 handler.command = /^(Vs8|vs8)$/i;
 export default handler;
-const _uptime = process.uptime() * 1000;
-  const uptime = clockString(_uptime);
 global.vs8 = `
 *⚜️ VS OFICIAL - 8 VS 8⚜️*
 
 _HORA :_
-•${uptime} 🇵🇪
-•${uptime} 🇦🇷🇨🇱
+•10 pm 🇵🇪
+•10 pm 🇦🇷🇨🇱
 
 *👑 ESCUADRA 1 👑*
 
