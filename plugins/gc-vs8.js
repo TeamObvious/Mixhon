924ed0382714c3399c924a18040c217b9ac9b0e7
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 11:49:16 -1200

    Update gc-vs8.js

--- plugins/gc-vs8.js
+++ plugins/gc-vs8.js
@@ -3,17 +3,13 @@ const handler = async (m, {conn}) => {
 };
 handler.command = /^(Vs8|vs8)$/i;
 export default handler;
-if (stopped === 'close' || !conn || !conn.user) return;
-  const status = global.db.data.settings[conn.user.jid] || {};
-  const _uptime = process.uptime() * 1000;
-  const uptime = clockString(_uptime);
 global.vs8 = `
 *⚜️ VS OFICIAL MASC - 8 VS 8 ⚜️*
 
 *Nota :* _La Hora Editenlo De Acuerdo El Vs_
 
 *HORA :*
-•10 pm 🇵🇪 ${uptime}
+•10 pm 🇵🇪
 •00 am 🇦🇷 🇨🇱
 
 *👑 ESCUADRA 1 👑*
