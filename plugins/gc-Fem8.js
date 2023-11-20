e96a7dde675bfd1f6fdb162a51be73cc0675218e
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 12:04:22 -1200

    Update and rename gc-vv2.js to gc-Fem8.js

---
+++ plugins/gc-Fem8.js
@@ -0,0 +1,37 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Vivido);
+};
+handler.command = /^(Vivido|vivido|Vv2|vv2)$/i;
+export default handler;
+
+global.Fem8= `
+🌸 VS OFICIAL FEM - 8 VS 8 🌸
+
+Nota : La Hora Editenlo De Acuerdo El Vs
+
+HORA :
+•10 pm 🇵🇪
+•00 am 🇦🇷 🇨🇱
+
+🌸 ESCUADRA 1 🌸
+
+🎀
+🎀
+🎀
+🎀
+
+🌸 ESCUADRA 2 🌸
+
+🎀
+🎀
+🎀
+🎀
+
+🌸 SUPLENTES 🌸
+
+🎀
+🎀
+🎀
+🎀
+
+`;
