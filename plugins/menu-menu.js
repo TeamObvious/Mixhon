954d3dc5c930d581016b060c336c58bccb868270
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-23 04:03:07 -0500

    Add files via upload

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -80,6 +80,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  *« JUEGOS »*
 
  ◤━━━━━━━━━━━◥
+ ►👺 _${usedPrefix}ship2
  ►👺 _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
  ►👺 _${usedPrefix}fake *<txt1> <@tag> <txt2>*_
  ►👺 _${usedPrefix}ppt *<papel / tijera /piedra>*_
