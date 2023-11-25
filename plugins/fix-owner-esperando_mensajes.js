f616ab804633898be74a2a3af0efe8a4039125b7
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-24 15:42:21 -1200

    Update fix-owner-esperando_mensajes.js

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -24,13 +24,13 @@ const handler = async (m, { conn, usedPrefix }) => {
     if (filesDeleted === 0) {
       await conn.sendMessage(m.chat, {text: '*🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*${filesDeleted} 🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `* Archivos Eliminaados ${filesDeleted} Por 🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
     await conn.sendMessage(m.chat, {text: '*🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `* 🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
