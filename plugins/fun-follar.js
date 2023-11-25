549a37cdbe108724afa4895ce68c931918e9cc85
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-24 21:09:10 -1200

    Update fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -3,15 +3,15 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
   const user = global.db.data.users[who];
-  if (!who) return m.reply(`*✳️ menciona a quien deseas abrazar*\n\n*📌 ejemplo :*\n${usedPrefix + command} @tag`);
+  if (!who) return m.reply(`*✳️ menciona a quien deseas follar*\n\n*📌 ejemplo :*\n${usedPrefix + command} @tag`);
 
 
-  const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  le esta dando un fuerte abrazo a @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
+  const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  se esta follando duro a @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
 
-  conn.sendMessage(m.chat, {react: {text: '🫂', key: abrazo.key}});
+  conn.sendMessage(m.chat, {react: {text: '👌🏼', key: abrazo.key}});
 };
-handler.help = ['abrazo @user'];
+handler.help = ['follar @user'];
 handler.tags = ['fun'];
-handler.command = ['abrazo'];
+handler.command = ['follar'];
 handler.group = true;
 export default handler;
