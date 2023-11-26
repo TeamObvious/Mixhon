60093a2b90c9d1e83e3903ab9969d0835fe96bdd
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-25 16:40:01 -1200

    📌 𝗠𝗜𝗫𝗛𝗢𝗡 📌
    📌 𝗠𝗜𝗫𝗛𝗢𝗡 📌

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝙀𝙡 𝘽𝙤𝙩 𝘾𝙤𝙢𝙥𝙧𝙖 𝘾𝙤𝙣 wa.me/51906528530 ${pesan}`;
-  let teks = `𝙀𝙮 𝙈𝙖𝙡𝙙𝙞𝙩𝙤 𝙉𝙚𝙜𝙧𝙭 𝙏𝙚 𝘼𝙣𝙤𝙩𝙖𝙨 𝙊 𝙏𝙚 𝙎𝙖𝙘𝙤 𝙃𝙞𝙟𝙭 𝘿𝙚 𝙋𝙪𝙩𝙖 \n\n ${oi}\n\n`;
+  let teks = `𝗟𝗹𝗲𝗻𝗲𝗻 𝗹𝗶𝘀𝘁𝗮 𝗺𝗮𝗻𝗰𝗼𝘀 𝗵𝗶𝗷𝘅 𝗱𝗲 𝗽𝘂𝘁𝗮 \n\n ${oi}\n\n`;
   for (const mem of participants) {
-    teks += `🦂| ▪ @${mem.id.split('@')[0]}\n`;
+    teks += `| ▪ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `\n\n🦂 𝘼𝙡𝙖𝙘𝙧𝙖𝙣 🦂`;
+  teks += `\n\n📌 𝗠𝗜𝗫𝗛𝗢𝗡 📌`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
